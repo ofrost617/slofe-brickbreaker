04aef4324b360e0561cbad9cad6af9dd86b47bad
Lizzie Hard <elizabeth_hard@hotmail.co.uk>
2017-09-25 16:40:25 +0100

    setting up nightwatch, phase-2-e is in node modules

---
+++ README.md
@@ -0,0 +1,6 @@
+Installing nightwatch:
+
+npm install -g nightwatch
+download latest version of selenium-server-standalone-{VERSION}.jar from http://selenium-release.storage.googleapis.com/index.html
+download chromedriver
+put both in bin folder
--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,11 +1,29 @@
-describe('Game', function() {
+// describe('Game', function() {
+//
+//   beforeEach(function() {
+//     game = new Phaser.Game
+//   })
+//
+//   describe("canvas is present", function() {
+//     it("has a canvas", function() {
+//       expect(game).toBe(true)
+//     });
+//   });
+// });
 
-  beforeEach(function() {
-    game = new BrickGame();
-  })
-
-
-
-
-
-})
+module.exports = {
+  'Demo test Google' : function (client) {
+    client
+      .url('http://www.google.com')
+      .waitForElementVisible('body', 1000)
+      .assert.title('Google')
+      .assert.visible('input[type=text]')
+      .setValue('input[type=text]', 'rembrandt van rijn')
+      .waitForElementVisible('button[name=btnG]', 1000)
+      .click('button[name=btnG]')
+      .pause(1000)
+      .assert.containsText('ol#rso li:first-child',
+        'Rembrandt - Wikipedia')
+      .end()
+  }
+}
